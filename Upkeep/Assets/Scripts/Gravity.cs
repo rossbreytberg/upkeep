e7962f19370ee63d93d5c183834988841614df89
rossbreytberg <rossbreytberg@gmail.com>
2018-10-10 21:40:23 -0700

    spacing

--- Upkeep/Assets/Scripts/Gravity.cs
+++ Upkeep/Assets/Scripts/Gravity.cs
@@ -7,9 +7,9 @@ public class Gravity : MonoBehaviour
     [SerializeField]
     protected Vector3 m_gravity = new Vector3(0, -9.81F, 0);
 
-	void Start () {
+    void Start () {
         Physics.gravity = gravity;
-	}
+    }
 
     public Vector3 gravity
     {
